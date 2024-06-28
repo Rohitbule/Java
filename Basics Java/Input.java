ac10e3a738988446d20e8f8fb1c9d099ae300fbe
Rohitbule <rohitbule539@gmail.com>
2024-06-28 20:21:52 +0530

    java Basics

---
+++ Basics Java/Input.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        //next,nextLine,nextInt,nextByte,nextfloat,nextDouble,nextBoolean,nextShort,nextLong
+        
+        /*String input = sc.next();
+        System.out.println(input);*/
+
+        /*String name = sc.nextLine();
+        System.out.println(name);*/
+
+        /*int number = sc.nextInt();
+        System.out.println(number);*/
+
+        /*float price = sc.nextFloat();
+        System.out.println(price);*/
+
+        /*boolean a = sc.nextBoolean();
+        System.out.print(a);*/
+
+        /*int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.print(sum);*/
+
+        /*int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.print(product);*/
+
+        /*int radius = sc.nextInt();
+        float area = 3.14f * radius * radius ;
+        System.out.print(area);*/
+
+        //TYPE CONVERSION = BYTE->SHORT->INT->FLOAT->LONG->DOUBLE
+        /*float number  = sc.nextInt();
+        System.out.println(number);*/
+
+        /*char a = 'a';
+        char b = 'b';
+        System.out.println((int)(a));
+        System.out.println((int)(b));
+        System.out.println(b-a);*/
+
+        byte b = 5;
+        byte a = (byte) (b*2);
+        System.out.println(a);
+    }
+}
---
+++ Basics Java/JavaBasics.java
@@ -0,0 +1,28 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+        /*System.out.print(" Hello Rohit");
+        System.out.println("Hello om!");
+        System.out.print("Hello Tushar \n");*/
+        
+        /*System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");*/
+
+        /*int a =10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        a=b;
+        System.out.println(a);*/
+
+        //byte=1byte,char=2char,short=2byte,boolean=1byte,int=4byte,long=8byte,float=4byte,double=8byte
+        //1byte => 8 bits.
+
+        /*int a = 10;
+        int b =20;
+        int sum = a + b;
+        System.out.println(sum);*/
+
+    }  
+}
