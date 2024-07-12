f997ffbb6fe32f5bb9268066e54fba3e2592f829
Rohitbule <rohitbule539@gmail.com>
2024-07-12 21:32:31 +0530

    Sorting

--- Sorting/bubble.java
+++ Sorting/bubble.java
@@ -1,33 +1,42 @@
-public class bubble {
-
-    public static void bublesort(int a[])
+import java.util.Arrays;
+import java.util.Collections;
+public class bubble 
+{
+    public static void bublesort(Integer arr[])
     {
-        int n=a.length;
+        int n=arr.length;
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<n-1-i; j++)
             {
-                if(a[j] > a[j+1])
+                if(arr[j] > arr[j+1])
                 {
-                    int temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         } 
-    }
+}
 
-    public static void printarr(int a[])
+    //printing an array
+    public static void printarr(Integer arr[])
     {
-        for(int i=0; i<a.length; i++)
+        for(int i=0; i<arr.length; i++)
         {
-            System.out.print(a[i]);
+            System.out.print(arr[i]);
         }
+        System.out.println();
     }
     public static void main(String args[])
     {
-        int a[] = { 5,4,1,3,2};
-        bublesort(a);
-        printarr(a);
+        Integer arr[] = { 5,4,1,3,2};
+        bublesort(arr);
+        printarr(arr);
+        //Arrays.sort(arr);
+        //printarr(arr);
+        
+        //Arrays.sort(arr,Collections.reverseOrder());
+        //printarr(arr);
     }
 }
